58391e28adf304c1a52a0b492a92d04f349d3915
Yazan Dabain <yazan.dabain@gmail.com>
2015-03-16 19:43:33 +0200

    Refactor main

--- source/app.d
+++ source/app.d
@@ -5,61 +5,102 @@ void main()
 {
 	auto psql = new PSQL("codename", "yazan", "127.0.0.1", 5432);
 	auto conn = psql.lockConnection();
-	auto id   = 0;
 
+	testGenericRowSelect(conn);
+	testSimpleDelete(conn);
+	testSimpleInsert(conn);
+	testTwoCommandsQuery(conn);
+	testTypedRowSelect(conn);
+	testHandleError(conn);
+}
+
+void testGenericRowSelect(Connection conn)
+{
+	writeln("QUERY: ", "SELECT * FROM tbl_people");
+	auto query = conn.query("SELECT * FROM tbl_people");
+
+	foreach (row; query.rows())
 	{
-		writeln("QUERY ", ++id, ": ", "SELECT * FROM tbl_people");
-		auto query = conn.query("SELECT * FROM tbl_people");
-
-		foreach (row; query.rows())
+		foreach (i, field; query.fields())
 		{
-			foreach (i, field; query.fields())
-			{
-				writeln(field.name, ": ", cast(char[]) row.columns[i]);
-			}
+			writeln(field.name, ": ", cast(char[]) row.columns[i]);
 		}
-
-		query.close();
-		writeln();
 	}
 
+	query.close();
+	writeln();
+}
+
+void testTwoCommandsQuery(Connection conn)
+{
+	writeln("QUERY: ", "SELECT * FROM tbl_people; SELECT * FROM tbl_people");
+	auto query = conn.query("SELECT * FROM tbl_people; SELECT * FROM tbl_people");
+
+	foreach (row; query.rows())
 	{
-		writeln("QUERY ", ++id, ": ", "SELECT * FROM tbl_people; SELECT * FROM tbl_people");
-		auto query = conn.query("SELECT * FROM tbl_people; SELECT * FROM tbl_people");
-
-		foreach (row; query.rows())
+		foreach (i, field; query.fields())
 		{
-			foreach (i, field; query.fields())
-			{
-				writeln(field.name, ": ", cast(char[]) row.columns[i]);
-			}
+			writeln(field.name, ": ", cast(char[]) row.columns[i]);
 		}
-
-		query.nextCommand();
-		foreach (row; query.rows())
-		{
-			foreach (i, field; query.fields())
-			{
-				writeln(field.name, ": ", cast(char[]) row.columns[i]);
-			}
-		}
-
-		query.close();
-		writeln();
 	}
 
+	query.nextCommand();
+	foreach (row; query.rows())
 	{
-		writeln("QUERY ", ++id, ": ", "SELECT * FROM tbl_people");
-		auto query = conn.query("SELECT * FROM tbl_people");
-
-		foreach (person; query.fill!Person())
+		foreach (i, field; query.fields())
 		{
-			writeln(person);
+			writeln(field.name, ": ", cast(char[]) row.columns[i]);
 		}
-
-		query.close();
-		writeln();
 	}
+
+	query.close();
+	writeln();
+}
+
+void testTypedRowSelect(Connection conn)
+{
+	writeln("QUERY: ", "SELECT * FROM tbl_people");
+	auto query = conn.query("SELECT * FROM tbl_people");
+
+	foreach (person; query.fill!Person())
+	{
+		writeln(person);
+	}
+
+	query.close();
+	writeln();
+}
+
+void testSimpleInsert(Connection conn)
+{
+	writeln("QUERY: ", "INSERT INTO tbl_people (name, password, email) VALUES ('test', '123', 'email@email.com')");
+	auto query = conn.query("INSERT INTO tbl_people (name, password, email) VALUES ('test', '123', 'email@email.com')");
+	query.close();
+	writeln();
+}
+
+void testSimpleDelete(Connection conn)
+{
+	writeln("QUERY: ", "DELETE FROM tbl_people WHERE name = 'test'");
+	auto query = conn.query("DELETE FROM tbl_people WHERE name = 'test'");
+	query.close();
+	writeln();
+}
+
+void testHandleError(Connection conn)
+{
+	writeln("QUERY: ", "INSERT INTO tbl_people (name, password, email) VALUES ('test', '123', 'email@email.com')");
+
+	SimpleQuery query;
+	try
+	{
+		query = conn.query("INSERT INTO tbl_people (name, password, email) VALUES ('test', '123', 'email@email.com')");
+	}
+	finally
+	{
+		query.close();
+	}
+	writeln();
 }
 
 struct Person
