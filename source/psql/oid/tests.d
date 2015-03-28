e5f0b63e447f3d3d592c551336eb1edfb4804813
Yazan Dabain <yazan.dabain@gmail.com>
2015-03-28 10:12:01 +0300

    Minor change to test for readability

--- source/psql/oid/tests.d
+++ source/psql/oid/tests.d
@@ -4,6 +4,6 @@ import
 	psql.oid;
 
 static assert(getSize!(int, FieldRepresentation.binary)(0) == int.sizeof);
-static assert(getSize!(int, FieldRepresentation.text)(0) == ubyte.sizeof);
+static assert(getSize!(int, FieldRepresentation.text)(0) == 1);
 static assert(getSize!(string, FieldRepresentation.text)("hello") == 5);
 static assert(getSize!(string, FieldRepresentation.binary)("hello") == 5);
